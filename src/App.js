96ddd080a3d67d36928e4a4f7ad9b56b894eb32f
AidanNgo <aidanngo88@gmail.com>
2025-02-05 00:41:10 -0600

    pretty much done? go to OH to confirm though

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import MenuItem from './components/MenuItem';
+import CafeLogo from './components/CafeLogo';
 
-// import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
 
 // Menu data. An array of objects where each object represents a menu item. Each menu item has an id, title, description, image name, and price.
 // You can use the image name to get the image from the images folder.
@@ -82,11 +84,35 @@ const menuItems = [
 function App() {
   return (
     <div>
-      <h1>Menu</h1>
-      <div className="menu">
-        {/* Display menu items dynamicaly here by iterating over the provided menuItems */}
-        <MenuItem title={menuItems[0].title} /> {/* Example for how to use a component */}
+      
+
+      <div className="logo col-12 d-flex align-items-center justify-content-center">
+        <CafeLogo />
       </div>
+
+      <div className="col d-flex align-items-center justify-content-center" id="motto-one">
+        The Best Food That Money Can Buy
+      </div>
+
+      <div className="col d-flex align-items-center justify-content-center" id="motto-two">
+        Made Fresh Everyday!
+      </div>
+     
+      <div className="row" id="menu">
+        {/* Display menu items dynamicaly here by iterating over the provided menuItems */}
+        {menuItems.map((item) => (
+
+          <MenuItem 
+          title={item.title}
+          description={item.description} 
+          imageName={item.imageName} 
+          price={item.price} 
+          />
+
+          ))}
+ 
+      </div>
+
     </div>
   );
 }
---
+++ src/components/CafeLogo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const CafeLogo = () => {
+    return <img src="/images/Titans-Cafe.png" alt="cafe logo" id="cafe-icon"/>;
+};
+
+
+export default CafeLogo;
